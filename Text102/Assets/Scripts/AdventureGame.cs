3a663f12116bbd1990d34ec670d7ed2632649196
Zenon Jakubiak <zjakubiak@hotmail.com>
2020-04-24 11:37:57 +0100

    nearly finished story game

---
+++ Text102/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,58 @@
+﻿// namespaces
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    // you need to use a specific namespace to have access to the user interface keyword
+    // available within inspector - so you can make changes
+    //[SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+    // need this in order to be able to drag the TMP object into the component
+    [SerializeField] TextMeshProUGUI storyText;
+
+
+      
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        //textComponent.fontSize = 40;
+        //textComponent.text = state.GetStateStory();
+        storyText.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        // shorthand when variable is declared and initialised
+        // the method returns an array of states, so var has context
+        var nextStates = state.GetNextStates();
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+        }
+
+        //textComponent.text = state.GetStateStory();
+        storyText.text = state.GetStateStory();
+    }
+}
---
+++ Text102/Assets/Scripts/State.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// creates a menu option when you are amending 
+[CreateAssetMenu(menuName = "State")]
+
+public class State : ScriptableObject
+{
+    // serialzefield allows access of variable within unity inspector
+    // 10 size of text box, 20 start to scroll when you hit this number of lines
+    [TextArea(10,20)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
